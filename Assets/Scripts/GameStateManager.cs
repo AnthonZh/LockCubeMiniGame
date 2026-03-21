8b9b1afe839504db2f3b1b46a7df582efbcded00
AnthonZh <anthonyzh3349@gmail.com>
2026-03-21 14:27:27 -0400

    Create Scripts for State Manager and Player Control

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField] public bool GameOn;
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameStateManager StateManager;
+    public Rigidbody2D PlayerBody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
